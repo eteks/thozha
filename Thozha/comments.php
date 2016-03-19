299d073886ec8a808113bf6dff20130cf6b011b0
spmuthu21 <muthu@globalensolutions.com>
2016-03-19 14:29:44 +0530

    change captcha design

--- Thozha/comments.php
+++ Thozha/comments.php
@@ -1,6 +1,6 @@
 
 <?php 
-	
+	session_start();
 	include('dbcon.php');
 	if(isset($_GET['post_comment'])){
 		$sql = "insert into comment (content,created,fullname,id,modified,parent,profile_picture_url,upvote_count) values ('".$_POST['content']."','".$_POST['created']."','".$_POST['fullname']."','".$_POST['id']."','".$_POST['modified']."','".$_POST['parent']."','".$_POST['profile_picture_url']."','".$_POST['upvote_count']."')";
@@ -14,5 +14,13 @@
 			$arr[] = $row; 
 		}		
 		print(json_encode($arr));
+	}
+	if(isset($_GET['captcha_check'])){
+		if(strtolower($_POST['code']) == strtolower($_SESSION['random_number'])){
+			echo "valid";	
+		}
+		else{
+			echo "invalid";
+		}
 	}
 ?>
